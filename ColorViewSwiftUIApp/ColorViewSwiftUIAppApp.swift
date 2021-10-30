26d292d4aeae4a03125f510951e7c966030b01fa
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-10-30 17:51:26 +0300

    Initial Commit

---
+++ ColorViewSwiftUIApp/ColorViewSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewSwiftUIAppApp.swift
+//  ColorViewSwiftUIApp
+//
+//  Created by Vahtee Boo on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
