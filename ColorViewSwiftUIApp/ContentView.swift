48f150406f7f502212a95eaec125d9a26861fde2
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-10-30 18:27:45 +0300

    Initial commit and Release Candidate :)

--- ColorViewSwiftUIApp/ContentView.swift
+++ ColorViewSwiftUIApp/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redSlider = Double.random(in: 0...1)
+    @State private var greenSlider = Double.random(in: 0...1)
+    @State private var blueSlider = Double.random(in: 0...1)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Color(.init(red: redSlider, green: greenSlider, blue: blueSlider, alpha: 1.0))
+                .frame(height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+            HStack {
+                Text("Red: \(redSlider)")
+                Text("Green: \(greenSlider)")
+                Text("Blue: \(blueSlider)")
+            }
+            ColorSlidersView(redValue: $redSlider, greenValue: $greenSlider, blueValue: $blueSlider)
+            Spacer()
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorSlidersView: View {
+    
+    @Binding var redValue: Double
+    @Binding var greenValue: Double
+    @Binding var blueValue: Double
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("0")
+                Slider(value: $redValue, in: 0...1)
+                Text("255")
+            }
+            HStack {
+                Text("0")
+                Slider(value: $greenValue, in: 0...1)
+                Text("255")
+            }
+            HStack {
+                Text("0")
+                Slider(value: $blueValue, in: 0...1)
+                Text("255")
+            }
+        }
     }
 }
